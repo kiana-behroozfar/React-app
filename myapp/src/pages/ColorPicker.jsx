f52595eaab33857e4d8bbb23bd41cff65988fe7f
kiana-behroozfar <92881001+kiana-behroozfar@users.noreply.github.com>
2022-07-24 22:17:31 +0430

    colorpicker finished

--- myapp/src/pages/ColorPicker.jsx
+++ myapp/src/pages/ColorPicker.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import{ColorPickerComponent} from '@syncfusion/ej2-react-inputs'
-import{Header} from '../components'
+import{Header} from '../components';
+
+const change =(args) => {
+  document.getElementById("preview").style.backgroundColor = args.currentValue.hex
+  change={change};
+}
+
+
 
 const ColorPicker = () => {
   return (
@@ -13,8 +20,24 @@ const ColorPicker = () => {
       <p className='text-2xl font-semibold mt-2 mb-4'>Inline Pallete</p>
       <ColorPickerComponent
       id='inline-pallete'
-      />
-    </div>
+      mode='Palette'
+      modeSwitcher={false}
+      inline
+      showButtons={false}
+      change={change}
+/>
+</div>
+    <div>
+      <p className='text-2xl font-semibold mt-2 mb-4'>Inline Picker</p>
+      <ColorPickerComponent
+      id='inline-pallete'
+      mode='Picker'
+      modeSwitcher={false}
+      inline
+      showButtons={false}
+      change={change}
+/>
+</div>
 
       </div>
      </div>
